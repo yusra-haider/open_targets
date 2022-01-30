3b2084358e809556c2e6707ec21b733c71bff054
Yusra Haider <yusra.haider@gmail.com>
2022-01-30 21:17:30 +0000

    cleaned up api

--- backend/app.py
+++ backend/app.py
@@ -2,25 +2,47 @@
 import json
 
 # --- third-party imports
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
 
-# todo: remove this?
 @app.route("/")
 def home():
-    return "<p>Open Target API</p>"
+    return "Open Targets API"
 
 
-@app.route("/get_all_data")
-def target_disease_associations():
+@app.route("/get_data")
+def get_target_disease_associations():
+    args = request.args
+    disease_id = args.get("disease_id")
+    limit = args.get("limit", 10, int)
+
     with open("data/associations.json") as json_file:
-        data = json.load(json_file)
-        return data
+        data = json.load(json_file)["data"]
 
-# todo: do we need a config file??
-# todo: creat readme
-# todo: idk why this isnt working with postman, it works fine in the browser
+        data = process_data(data, disease_id, limit)
+        return {"data": data}
+
+
+def process_data(data, disease_id, limit):
+    # default sort for data --> desc order by association_score
+    data = sorted(data, key=lambda d: d["association_score"]["overall"], reverse=True)
+
+    # filter by disease id
+    if disease_id:
+        for d in data:
+            print(d.get("disease").get("id") == disease_id)
+        data = [d for d in data if d.get("disease", {}).get("id", "") == disease_id]
+
+    # limit data
+    if limit:
+        data = data[:limit]
+    return data
+
+
+
+
+
